1b3ff5fc4a725cf715f6dcd82e41d1062170b608
Jadelrea <jalonso@ibv.csic.es>
2023-01-20 10:30:05 +0100

    update200123

--- random100_short/iq-tree_anc_asr.sh
+++ random100_short/iq-tree_anc_asr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/
 
-nice -n 5 iqtree -s run_alignment_no_resis.fas \
+nice -n 5 iqtree -s run_alignment_no_resis_no_clus.fas \
             -te iq_tree.contree -T AUTO --threads-max 3 \
             --prefix iq_tree_anc_asr -asr -m GTR
